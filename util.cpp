ca2cdfb6a72baf31f6da71c088ed22ba77ecdce6
Daniel Chen <dchen435@usc.edu>
2025-10-03 04:36:57 +0000

    implemented parseStringToWords()

--- util.cpp
+++ util.cpp
@@ -15,15 +15,63 @@ std::string convToLower(std::string src)
     to a set of words based on the criteria given in the assignment **/
 std::set<std::string> parseStringToWords(string rawWords)
 {
+    std::set<std::string> keyWords;
 
+    std::string prePunc;
+    std::string postPunc;
+    bool foundPunc = false;
 
+    for (unsigned char uch : rawWords) {
+        if (std::isalnum(uch)) {
+            char lc = static_cast<char>(std::tolower(uch));
+            if (!foundPunc) {
+                prePunc.push_back(lc);
+            } else {
+                postPunc.push_back(lc);
+            }
+        } else if (std::ispunct(uch)) {
+            if (!foundPunc) {
+                if (prePunc.size() >= 2) {
+                    keyWords.insert(prePunc);
+                }
+                prePunc.clear();
+                foundPunc = true;
+            } else {
+                if (postPunc.size() >= 2) {
+                    keyWords.insert(postPunc);
+                }
+                postPunc.clear();
+            }
+        } else {
+            if (!foundPunc) {
+                if (prePunc.size() >= 2) {
+                    keyWords.insert(prePunc);
+                }
+                prePunc.clear();
+            } else {
+                if (postPunc.size() >= 2) {
+                    keyWords.insert(postPunc);
+                }
+                postPunc.clear();
+            }
+        }
+    }
+    
+    // final flush
+    if (!foundPunc) {
+        if (prePunc.size() >= 2) {
+            keyWords.insert(prePunc);
+        }
+    } else {
+        if (postPunc.size() >= 2) {
+            keyWords.insert(postPunc);
+        }
+        if (prePunc.size() >= 2) {
+            keyWords.insert(prePunc);
+        }
+    }
 
-
-
-
-
-
-
+    return keyWords;
 
 }
 
